f43380188c3cc47caf4daddaef64c10d14ce7eca
Massimiliano Bonomi <max@eduroam-262-2-149.epfl.ch>
2023-09-07 11:08:27 +0200

    clean water

--- tutorials/4-ensemble-wat/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
+++ tutorials/4-ensemble-wat/1-Analysis/prepare_PLUMED_Ensemble_analysis.sh
@@ -5,4 +5,4 @@ gmx_mpi trjcat -f ../0-Production/rep-*/traj*.xtc -cat -o traj-all.xtc
 plumed driver --plumed plumed_fix_pbc.dat --mf_xtc traj-all.xtc
 
 # 3) extract from step3_input_xtc.pdb only the atoms used to generate the cryo-em map. 
-gmx_mpi trjconv -f ../../3-refinement-wat/3-Map-Scaling/step3_input_xtc.pdb -s ../0-Production/rep-00/production.tpr -n ../../3-refinement-wat/0-Building/index.ndx -o conf_map.pdb
+echo System-MAP | gmx_mpi trjconv -f ../../3-refinement-wat/3-Map-Scaling/step3_input_xtc.pdb -s ../0-Production/rep-00/production.tpr -n ../../3-refinement-wat/0-Building/index.ndx -o conf_map.pdb
